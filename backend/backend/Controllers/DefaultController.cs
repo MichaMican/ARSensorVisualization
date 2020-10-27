150acb01ab8b13e6e3fdfec74ab2f334e5b00524
Michael Mican <mican.micha@gmail.com>
2020-10-27 14:50:53 +0100

    add v2 Endpoint; add percentage range definition

--- backend/backend/Controllers/DefaultController.cs
+++ backend/backend/Controllers/DefaultController.cs
@@ -16,16 +16,14 @@ namespace backend.Controllers
         Random rnd = new Random();
         private const int MAX_VECTOR = 10000;
 
-        [HttpGet("data")]
-        public ActionResult<List<VectorDto>> GetVectorCollection(int limit = MAX_VECTOR, int offset = 0, int? minY = null, int? maxY = null)
-        {
+
+        private List<VectorDto> GenerateVectors(int vectorCount) {
 
             var returnList = new List<VectorDto>();
-            var count = MAX_VECTOR;
 
             double vectorLength = rnd.Next(0, 10) / 10f;
 
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < vectorCount; i++)
             {
                 var vector = new VectorDto()
                 {
@@ -40,6 +38,41 @@ namespace backend.Controllers
                 returnList.Add(vector);
             }
 
+            return returnList;
+        }
+
+        [HttpGet("data/v2")]
+        public ActionResult<List<VectorDto>> GetVectorCollectionV2(int limit = MAX_VECTOR, double minY = 0.0, double maxY = 1.0, double minX = 0.0, double maxX = 1.0)
+        {
+            var returnList = GenerateVectors(MAX_VECTOR);
+
+            var maxXVecValue = returnList.Max((vec) => { return vec.x; });
+            var maxYVecValue = returnList.Max((vec) => { return vec.y; });
+            var minXVecValue = returnList.Min((vec) => { return vec.x; });
+            var minYVecValue = returnList.Min((vec) => { return vec.y; });
+
+            double xMaxBorder = maxX * (maxXVecValue - minXVecValue) + minXVecValue;
+            double yMaxBorder = maxY * (maxYVecValue - minYVecValue) + minYVecValue;
+            double xMinBorder = minX * (maxXVecValue - minXVecValue) + minXVecValue;
+            double yMinBorder = minY * (maxYVecValue - minYVecValue) + minYVecValue;
+
+            returnList = returnList.Where((vector) => { return vector.y >= yMinBorder && vector.y <= yMaxBorder; }).ToList();
+            returnList = returnList.Where((vector) => { return vector.x >= xMinBorder && vector.x <= xMaxBorder; }).ToList();
+
+            if (returnList.Count > limit)
+            {
+                returnList.RemoveRange(limit, returnList.Count - limit);
+            }
+
+            return returnList;
+        }
+
+        [HttpGet("data")]
+        public ActionResult<List<VectorDto>> GetVectorCollection(int limit = MAX_VECTOR, int offset = 0, int? minY = null, int? maxY = null, int? minX = null, int? maxX = null)
+        {
+            var returnList = GenerateVectors(MAX_VECTOR);
+
+            //Filter Y axis
             if(minY != null && maxY != null)
             {
                 returnList = returnList.Where((vector) => { return vector.y >= minY && vector.y <= maxY; }).ToList();
@@ -52,8 +85,23 @@ namespace backend.Controllers
             {
                 returnList = returnList.Where((vector) => { return vector.y <= maxY; }).ToList();
             }
-            
-            if(offset != 0)
+
+            //Filter X axis
+            if (minX != null && maxX != null)
+            {
+                returnList = returnList.Where((vector) => { return vector.x >= minX && vector.x <= maxX; }).ToList();
+            }
+            else if (minX != null)
+            {
+                returnList = returnList.Where((vector) => { return vector.x >= minX; }).ToList();
+            }
+            else if (maxX != null)
+            {
+                returnList = returnList.Where((vector) => { return vector.x <= maxX; }).ToList();
+            }
+
+            //Convert output to fullfill offset & limit
+            if (offset != 0)
             {
                 if(offset >= returnList.Count)
                 {
@@ -64,7 +112,6 @@ namespace backend.Controllers
                     returnList.RemoveRange(0, offset);
                 }
             }
-
             if(returnList.Count > limit)
             {
                 returnList.RemoveRange(limit, returnList.Count - limit);
